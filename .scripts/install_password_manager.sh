7bc0a331bff92245d18ab18d858d2305fb6bf01d
Jamie Hitchen <25344048+jmehitch@users.noreply.github.com>
2025-02-20 15:27:04 +0000

    move packages

--- .scripts/install_password_manager.sh
+++ .scripts/install_password_manager.sh
@@ -3,16 +3,7 @@
 # exit immediately if bitwarden-cli is already in $PATH
 type bw >/dev/null 2>&1 && exit
 
-# commands to install bitwarden-cli on Darwin or Linu
-case "$(uname -s)" in
-Darwin)
-    mise use -g npm:@bitwarden/cli
-    ;;
-Linux)
-    mise use -g npm:@bitwarden/cli
-    ;;
-*)
-    echo "unsupported OS"
-    exit 1
-    ;;
-esac
+mise use -g node
+mise use -g npm:@bitwarden/cli
+bw config server https://vault.bitwarden.eu
+bw login
--- run_once_github_keys.sh
+++ run_once_github_keys.sh
@@ -2,13 +2,15 @@
 
 gh auth login -h github.com -s admin:public_key -s write:gpg_key
 
-bw get item ssh-key | jq -r '.sshKey.publicKey' > ~/.ssh/github.pub
-bw get item ssh-key | jq -r '.sshKey.privateKey' > ~/.ssh/github
-gh ssh-key add -t "$(hostname)" ~/.ssh/github.pub
+bw get item ssh-key | jq -r '.sshKey.publicKey' > ~/.ssh/id_ed25519.pub
+bw get item ssh-key | jq -r '.sshKey.privateKey' > ~/.ssh/id_ed25519
+gh ssh-key add -t jamie ~/.ssh/id_ed25519.pub
 
 bw get notes github-gpg-key > private.key
 gpg --import private.key
 rm private.key
 gpg --armor --export $(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{print $2}' | cut -d'/' -f2) > public.gpg
-gh gpg-key add -t "$(hostname)" public.gpg
+if $(gh gpg-key list | grep $(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{print $2}' | cut -d'/' -f2) | wc -l) -eq 0; then
+    gh gpg-key add -t jamie public.gpg
+fi
 rm public.gpg
